95f4156565979974e6373708d3b1b1157f1694cb
Yoshio Chorus <yoshimintos@fedora-yosh.fritz.box>
2026-05-09 18:31:49 +0200

    feat: Keyboard-Shortcuts (Ctrl+Q, Space, Escape)

--- src/main.py
+++ src/main.py
@@ -31,7 +31,27 @@ class ZungenTrainerApp(Adw.Application):
         win = self.props.active_window
         if not win:
             win = ZungenTrainerWindow(application=self)
+            self._setup_shortcuts(win)
         win.present()
+
+    def _setup_shortcuts(self, win):
+        # Ctrl+Q: Beenden
+        quit_action = Gio.SimpleAction.new("quit", None)
+        quit_action.connect("activate", lambda *_: self.quit())
+        self.add_action(quit_action)
+        self.set_accels_for_action("app.quit", ["<Control>q"])
+
+        # Space: Training starten/stoppen
+        toggle_action = Gio.SimpleAction.new("toggle-training", None)
+        toggle_action.connect("activate", lambda *_: win.toggle_training())
+        win.add_action(toggle_action)
+        self.set_accels_for_action("win.toggle-training", ["space"])
+
+        # Escape: Training abbrechen
+        stop_action = Gio.SimpleAction.new("stop-training", None)
+        stop_action.connect("activate", lambda *_: win.training_page.stop_training())
+        win.add_action(stop_action)
+        self.set_accels_for_action("win.stop-training", ["Escape"])
 
 
 def main():
